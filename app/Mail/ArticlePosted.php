becc89260094eca6f844ba41493c47bf4e66eefd
tjanuki <t.januki@gmail.com>
2024-04-23 17:55:17 -0400

    Modify mail title

--- app/Mail/ArticlePosted.php
+++ app/Mail/ArticlePosted.php
@@ -4,7 +4,6 @@ namespace App\Mail;
 
 use App\Models\Article;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
@@ -28,7 +27,7 @@ class ArticlePosted extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Jareamaga: "'.$this->article->title.'"',
+            subject: "Jareamaga: {$this->article->title}",
         );
     }
 
